927bc3fa243eb46f21ba1f01618724a44a9afb2c
Valentin <valent.chamboredon@gmail.com>
2025-03-25 11:46:08 +0100

    tp 3

--- assets/app.js
+++ assets/app.js
@@ -6,6 +6,7 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './vendor/bootstrap/dist/css/bootstrap.min.css'
+import './vendor/bootstrap/bootstrap.index.js';
 import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
