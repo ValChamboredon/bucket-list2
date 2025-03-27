5151ad9df6b7581025bef9bc8311791a5861d20e
Valentin <valent.chamboredon@gmail.com>
2025-03-27 11:13:51 +0100

    tp05

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,15 +11,23 @@ final class WishController extends AbstractController
 {
 
     #[Route('/wishes',name:'app_wish_list')]
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
-        return $this->render('wish/list.html.twig',
-            ['wishes' => ['Learn Symfony', 'Learn Docker', 'Learn API Platform', 'Master JavaScript', 'Master React']]);
+        $wishes = $wishRepository->findBy(['isPublished' => true], ['dateCreated' => 'DESC']);
+
+        return $this->render('wish/list.html.twig', [
+            'wishes' => $wishes,
+        ]);
     }
 
-    #[Route('/wishes/{id}',name:'app_wish_detail',requirements: ['id' => '\d+'])]
-    public function detail(int $id): Response
+    #[Route('/wishes/{id}', name: 'app_wish_detail', requirements: ['id' => '\d+'])]
+    public function detail(int $id, WishRepository $wishRepository): Response
     {
-        return $this->render('wish/detail.html.twig', ['id' => $id]);
+
+        $wish = $wishRepository->find($id);
+
+        return $this->render('wish/detail.html.twig', [
+            'wish' => $wish,
+        ]);
     }
 }
